ee30550430f2b8dc8be4d9df7b354b9499c23dae
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-02-09 19:42:57 +0900

    Time: 0 ms (100%), Space: 55.8 MB (79.51%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -0,0 +1,22 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    let idx1 = m - 1;
+    let idx2 = n - 1;
+    let full = m + n;
+
+    while (idx1 >= 0 && idx2 >= 0) {
+        if (nums1[idx1] >= nums2[idx2]) {
+            nums1[idx1 + idx2 + 1] = nums1[idx1];
+            --idx1;
+        } else {
+            nums1[idx1 + idx2 + 1] = nums2[idx2];
+            --idx2;
+        }
+    }
+    while (idx2 >=0) {
+        nums1[idx1 + idx2 + 1] = nums2[idx2];
+        --idx2;
+    }
+};
