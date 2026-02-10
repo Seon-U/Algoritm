57d24f75fa8dc221bc53dce38d684df6437788e2
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-02-10 11:07:45 +0900

    Time: 0 ms (100%), Space: 55.7 MB (38.28%) - LeetHub

--- 0027-remove-element/0027-remove-element.ts
+++ 0027-remove-element/0027-remove-element.ts
@@ -2,11 +2,10 @@ function removeElement(nums: number[], val: number): number {
     let k = 0;
     let max = nums.length;
     for (let i=0; i<max; i++) {
-        if (nums[i] == val) { 
-            nums[i] = 52; 
+        if (nums[i] != val) { 
+            nums[k] = nums[i] 
             k++;
         };
     }
-    nums = nums.sort((a, b) => a - b);
-    return max - k;
+    return k;
 };
