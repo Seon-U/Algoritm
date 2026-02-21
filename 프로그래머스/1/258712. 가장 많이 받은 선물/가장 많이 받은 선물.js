e9cb791bd62f57e5c6064c8396065b4a2dcee600
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-02-21 12:50:50 +0900

    Add logic to calculate most received gifts
    Implement gift distribution logic to determine the most received gift.

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,4 +1,44 @@
 function solution(friends, gifts) {
-    var answer = 0;
-    return answer;
+    const n = friends.length;
+    
+    const index = {};
+    for (let i = 0; i<n; i++) {
+        index[friends[i]] = i;
+    }
+    
+    const give = Array.from({length: n}, () => new Array(n).fill(0));
+    const giftScore = new Array(n).fill(0);
+    
+    for (let i=0; i<gifts.length; i++) {
+        const spaceIdx = gifts[i].indexOf(" ");
+        
+        const from = index[gifts[i].slice(0, spaceIdx)];
+        const to = index[gifts[i].slice(spaceIdx + 1)];
+        
+        give[from][to]++;
+        giftScore[from]++;
+        giftScore[to]--;
+    }
+    
+    const next = new Array(n).fill(0);
+    
+    for (let i=0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            const ij = give[i][j];
+            const ji = give[j][i];
+            
+            if (ij > ji) {
+                next[i]++;
+            } else if (ij < ji) {
+                next[j]++;
+            } else {
+                if (giftScore[i] > giftScore[j]) {
+                    next[i]++;
+                } else if (giftScore[i] < giftScore[j]) {
+                    next[j]++;
+                }
+            }
+        }
+    }
+    return Math.max(...next);
 }
