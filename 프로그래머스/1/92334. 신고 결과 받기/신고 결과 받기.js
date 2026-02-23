a99d6bd04da3caa6aaf0afac9d855834b6a4e1db
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-02-23 20:14:18 +0900

    [level 1] Title: 신고 결과 받기, Time: 657.93 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -0,0 +1,18 @@
+function solution(id_list, report, k) {
+    const n = id_list.length;
+    const suedIdx_sue = Array.from({length: n}, () => new Set());
+    const mailedCnt = Array.from({length: n}, () => 0);
+    for (const rep of report) {
+        const [sue, sued] = rep.split(" ");
+        const idx = id_list.indexOf(sued);
+        suedIdx_sue[idx].add(sue);
+    }
+    for (let i=0; i<n ;i++) {
+        if (suedIdx_sue[i].size >= k) {
+            for (const suer of suedIdx_sue[i]) {
+                mailedCnt[id_list.indexOf(suer)]++;
+            }
+        }
+    }
+    return mailedCnt;
+}
