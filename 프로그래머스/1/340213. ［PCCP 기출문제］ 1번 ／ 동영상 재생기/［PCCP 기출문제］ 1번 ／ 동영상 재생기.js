53b9b6291c5f33e008d4d54a83edb08d5b2cf7fd
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-02-24 15:05:34 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.38 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -4,11 +4,6 @@ function solution(video_len, pos, op_start, op_end, commands) {
         return min * 60 + sc;
     }
     
-    const sc_videoEnd = makeSc(video_len);
-    const sc_opStart = makeSc(op_start);
-    const sc_opEnd = makeSc(op_end);
-    let scPos = makeSc(pos);
-    
     const makeTimeString = (sc) => {
         const min = Math.floor(sc / 60).toString().padStart(2, "0");
         sc = (sc % 60).toString().padStart(2, "0");
@@ -19,15 +14,18 @@ function solution(video_len, pos, op_start, op_end, commands) {
         if (scPos >= sc_opStart && scPos < sc_opEnd) scPos = sc_opEnd;
     }
     
+    const clamp = (value, min, max) => Math.max(min, Math.min(max, value));
+    
+    const sc_videoEnd = makeSc(video_len);
+    const sc_opStart = makeSc(op_start);
+    const sc_opEnd = makeSc(op_end);
+    let scPos = makeSc(pos);
+    
     skipOp();
     
     for (const cmd of commands) {
-        if (cmd == "prev") {
-            scPos = (scPos - 10) < 0 ? 0 : scPos - 10;
-        } else {
-            scPos = scPos + 10 > sc_videoEnd ? 
-                sc_videoEnd : scPos + 10;
-        }
+        const diff = cmd == "prev" ? -10 : 10;
+        scPos = clamp(scPos + diff, 0, sc_videoEnd);
         skipOp();
     }
     
