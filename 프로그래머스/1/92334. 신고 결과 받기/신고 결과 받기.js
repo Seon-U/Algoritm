cb5dd491d1aa00d7fc695760ded43d7b75d24e3c
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-02-24 09:05:35 +0900

    [level 1] Title: 신고 결과 받기, Time: 212.10 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,26 +1,24 @@
 function solution(id_list, report, k) {
-    const n = id_list.length;
-    
-    //1. indexOf(n)생략 용 Obj
-    const idToIndex = {};
-    id_list.forEach((id, i) => {
-        idToIndex[id] = i;
+    const reportedMap = new Map();
+    const mailMap = new Map();
+
+    id_list.forEach(id => {
+        reportedMap.set(id, new Set());
+        mailMap.set(id, 0);
     });
-    
-    const suedIdx_sue = Array.from({length: n}, () => new Set());
-    const mailedCnt = new Array(n).fill(0);
-    
-    for (const rep of report) {
-        const [sue, sued] = rep.split(" ");
-        suedIdx_sue[idToIndex[sued]].add(sue);
+
+    for (const r of report) {
+        const [reporter, reported] = r.split(" ");
+        reportedMap.get(reported).add(reporter);
     }
-    
-    for (let i=0; i<n ;i++) {
-        if (suedIdx_sue[i].size >= k) {
-            for (const suer of suedIdx_sue[i]) {
-                mailedCnt[id_list.indexOf(suer)]++;
+
+    for (const [user, reporters] of reportedMap) {
+        if (reporters.size >= k) {
+            for (const reporter of reporters) {
+                mailMap.set(reporter, mailMap.get(reporter) + 1);
             }
         }
     }
-    return mailedCnt;
+
+    return id_list.map(id => mailMap.get(id));
 }
