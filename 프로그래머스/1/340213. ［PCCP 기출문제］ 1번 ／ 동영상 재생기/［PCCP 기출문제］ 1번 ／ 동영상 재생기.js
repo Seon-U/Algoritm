d89d627709d43daaff77bb95288858d907b20ece
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-02-24 14:58:47 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -4,32 +4,32 @@ function solution(video_len, pos, op_start, op_end, commands) {
         return min * 60 + sc;
     }
     
-    const makeTimeString = (sc) => {
-        let min = Math.floor(sc / 60);
-        min = min < 10 ? `0${min}` : min;
-        sc = sc % 60;
-        sc = sc < 10 ? `0${sc}` : sc;
-        return `${min}:${sc}`;
-    }
-    
     const sc_videoEnd = makeSc(video_len);
     const sc_opStart = makeSc(op_start);
     const sc_opEnd = makeSc(op_end);
     let scPos = makeSc(pos);
     
-    if (scPos >= sc_opStart && scPos < sc_opEnd) {
-        scPos = sc_opEnd;
-    };
+    const makeTimeString = (sc) => {
+        const min = Math.floor(sc / 60).toString().padStart(2, "0");
+        sc = (sc % 60).toString().padStart(2, "0");
+        return `${min}:${sc}`;
+    }
+    
+    const skipOp = () => {
+        if (scPos >= sc_opStart && scPos < sc_opEnd) scPos = sc_opEnd;
+    }
+    
+    skipOp();
     
     for (const cmd of commands) {
         if (cmd == "prev") {
-            (scPos - 10) < 0 ? scPos = 0 : scPos -= 10;
+            scPos = (scPos - 10) < 0 ? 0 : scPos - 10;
         } else {
-            scPos + 10 > sc_videoEnd ? scPos = sc_videoEnd : scPos += 10;
+            scPos = scPos + 10 > sc_videoEnd ? 
+                sc_videoEnd : scPos + 10;
         }
-        if (scPos >= sc_opStart && scPos < sc_opEnd) {
-            scPos = sc_opEnd;
-        }
+        skipOp();
     }
+    
     return makeTimeString(scPos);
 }
