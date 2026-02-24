b400f0e4061c9519f8760df5459a7dd32e6dc980
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-02-24 09:01:29 +0900

    [level 1] Title: 신고 결과 받기, Time: 393.80 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,12 +1,20 @@
 function solution(id_list, report, k) {
     const n = id_list.length;
+    
+    //1. indexOf(n)생략 용 Obj
+    const idToIndex = {};
+    id_list.forEach((id, i) => {
+        idToIndex[id] = i;
+    });
+    
     const suedIdx_sue = Array.from({length: n}, () => new Set());
-    const mailedCnt = Array.from({length: n}, () => 0);
+    const mailedCnt = new Array(n).fill(0);
+    
     for (const rep of report) {
         const [sue, sued] = rep.split(" ");
-        const idx = id_list.indexOf(sued);
-        suedIdx_sue[idx].add(sue);
+        suedIdx_sue[idToIndex[sued]].add(sue);
     }
+    
     for (let i=0; i<n ;i++) {
         if (suedIdx_sue[i].size >= k) {
             for (const suer of suedIdx_sue[i]) {
