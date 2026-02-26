51cfb5fbb5d8c5642dba22ca4600012a5f45cc79
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-02-26 18:12:46 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.24 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,26 +1,21 @@
 function solution(bandage, health, attacks) {
     const [skillTime, healSize, bonusHeal] = bandage;
-    const attTime_damage = new Map();
-    const lastAttTime = attacks[attacks.length - 1][0];
     const maxHealth = health;
     
-    attacks.forEach((v) => attTime_damage.set(v[0], v[1]));
-    
-    let healCnt = 0;
-    for (let cTime = 1; cTime <= lastAttTime; cTime++) {
-        if (attTime_damage.get(cTime) != null) {
-            health -= attTime_damage.get(cTime);
-            healCnt = 0;
-            if (health <= 0) return -1;
-        } else {
-            health = Math.min(health + healSize, maxHealth);
-            healCnt++;
-            if (healCnt == skillTime) {
-                health = Math.min(health + bonusHeal, maxHealth);
-                healCnt = 0;
-            };
+    let formerAttTime = 0;
+    for (const [attTime, damage] of attacks) {
+        const diffTime = attTime - formerAttTime - 1;
+        
+        if (diffTime > 0) {            
+            const bonus = Math.floor(diffTime / skillTime) * bonusHeal;
+            const defHeal = (diffTime) * healSize;
+            health = Math.min(health + bonus + defHeal, maxHealth);
         }
+        
+        formerAttTime = attTime;
+        health -= damage;
+        
+        if (health <= 0) return -1;        
     }
-    
     return health;
 }
