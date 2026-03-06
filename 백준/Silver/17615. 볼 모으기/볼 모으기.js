032d25ba574b071214d4860e786498035294eea0
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-03-06 16:37:18 +0900

    백준/Silver/17615 다시풀기  1

--- 백준/Silver/17615. 볼 모으기/볼 모으기.js
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.js
@@ -10,28 +10,23 @@ const answer = (n, arr) => {
 
     arr[0] == "R" ? blueArr.push(0) : redArr.push(0);
 
-    let cnt = 0;
-    for (let i=0; i < arr.length - 1; i++) {
-        cnt++;
+    let cnt = 1;
+    for (let i=0; i < n - 1; i++) {
         if (arr[i] != arr[i+1]) {
             arr[i] == "R" ? redArr.push(cnt) : blueArr.push(cnt);
-            cnt = 0;
-            if (i + 1 === arr.length - 1) arr[i + 1] == "R" ? redArr.push(1) : blueArr.push(1);
-        } else if (i + 1 === arr.length - 1) {
+            cnt = 1;
+        } else {
             cnt++;
-            arr[i + 1] == "R" ? redArr.push(cnt) : blueArr.push(cnt);
         }
     }
 
-    arr[arr.length - 1] == "R" ? blueArr.push(0) : redArr.push(0);
+    arr[n - 1] == "R" ? redArr.push(cnt) : blueArr.push(cnt);
+
+    arr[n - 1] == "R" ? blueArr.push(0) : redArr.push(0);
 
     const getMinCnt = (arr) => {
         const skipIdx = arr[0] >= arr[arr.length - 1] ? 0 : arr.length - 1;
-        let answer = 0;
-        for (let i=0; i<arr.length; i++) {
-            if (i != skipIdx) answer += arr[i];
-        }
-        return answer;
+        return arr.reduce((acc, v,i) => i === skipIdx ? acc : acc + v, 0);
     }
     
     const redMin = getMinCnt(redArr);
