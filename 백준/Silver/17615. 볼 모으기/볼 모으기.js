06a159aa7f7f648d572fbb9952ec931ad783bb79
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-03-06 17:06:38 +0900

    백준/Silver/17615 다시 풀기

--- 백준/Silver/17615. 볼 모으기/볼 모으기.js
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.js
@@ -1,49 +1,51 @@
 const rl = require('readline');
-
 const line = rl.createInterface(process.stdin, process.stdout);
 let args = [];
-let cnt = 0;
 
-const answer = (n, arr) => {
-    const redArr = [];
-    const blueArr = [];
+const answer = (n, str) => {
+    let rCount = 0, bCount = 0;
+    for (const c of str) c === 'R' ? rCount++ : bCount++;
 
-    arr[0] == "R" ? blueArr.push(0) : redArr.push(0);
+    let startLen = 0, endLen = 0;
+    const startColor = str[0];
+    const endColor = str[str.length - 1];
 
-    let cnt = 1;
-    for (let i=0; i < n - 1; i++) {
-        if (arr[i] != arr[i+1]) {
-            arr[i] == "R" ? redArr.push(cnt) : blueArr.push(cnt);
-            cnt = 1;
-        } else {
-            cnt++;
-        }
+    for (let i = 0; i < str.length && str[i] === startColor; i++) startLen++;
+    for (let i = str.length - 1; i >= 0 && str[i] === endColor; i--) endLen++;
+
+    let ans = Infinity;
+
+    if (startColor === 'R') {
+        ans = Math.min(ans, rCount - startLen);
+        ans = Math.min(ans, bCount - 0);       
+    } else {
+        ans = Math.min(ans, bCount - startLen);
+        ans = Math.min(ans, rCount - 0);
     }
 
-    arr[n - 1] == "R" ? redArr.push(cnt) : blueArr.push(cnt);
-
-    arr[n - 1] == "R" ? blueArr.push(0) : redArr.push(0);
-
-    const getMinCnt = (arr) => {
-        const skipIdx = arr[0] >= arr[arr.length - 1] ? 0 : arr.length - 1;
-        return arr.reduce((acc, v,i) => i === skipIdx ? acc : acc + v, 0);
+    if (endColor === 'R') {
+        ans = Math.min(ans, rCount - endLen);
+        ans = Math.min(ans, bCount - 0);
+    } else {
+        ans = Math.min(ans, bCount - endLen);
+        ans = Math.min(ans, rCount - 0);
     }
-    
-    const redMin = getMinCnt(redArr);
-    const blueMin = getMinCnt(blueArr);
-    return Math.min(redMin, blueMin);
-}
 
-
-line.on("line",
-    (v) => {
-        args.push(v);
-        cnt++;
-        if (cnt >= 2) line.close();
+    if (startColor === endColor) {
+        const skipLen = Math.max(startLen, endLen);
+        const targetCount = startColor === 'R' ? rCount : bCount;
+        ans = Math.min(ans, targetCount - skipLen);
     }
-);
+
+    return ans;
+};
+
+line.on("line", (v) => {
+    args.push(v.trim());
+    if (args.length >= 2) line.close();
+});
 
 line.on("close", () => {
-    console.log(answer(args[0], args[1]));
+    console.log(answer(+args[0], args[1]));
     process.exit();
 });
