dcad9a5b3135004bf2774697d24b9a6a8090124e
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-08-19 20:58:23 +0900

    [level 3] Title: 베스트앨범, Time: 0.23 ms, Memory: 44.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,35 @@
+// 10 ^ 4, n2 로직 허용
+function solution(genres, plays) {
+    const genreList = {};
+    const album = [];
+    
+    for (let i=0; i < genres.length; i++) {
+        const genre = genres[i];
+        if (genreList[genre] === undefined) {
+            genreList[genre] = {
+                total: plays[i],
+                firstBest: i,
+                secondBest: null,
+            }
+        } else {
+            const { _, firstBest, secondBest } = genreList[genre];
+            genreList[genre].total += plays[i];
+            
+            if (plays[i] > plays[firstBest]) {
+                genreList[genre].firstBest = i;
+                genreList[genre].secondBest = firstBest;
+            } else if (secondBest === null || plays[i] > plays[secondBest]) {
+                genreList[genre].secondBest = i;
+            }
+        }
+    }
+    
+    const List = Object.entries(genreList).sort((a, b) => b[1].total - a[1].total); 
+    
+    List.forEach((v) => {
+        album.push(v[1].firstBest);
+        if (v[1].secondBest !== null) album.push(v[1].secondBest);
+    })
+        
+    return album;
+}
