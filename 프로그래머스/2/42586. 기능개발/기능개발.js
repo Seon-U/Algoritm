b22158d1e2470a3ee0ede90be4fe07d124f8d840
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-08-19 22:32:31 +0900

    [level 2] Title: 기능개발, Time: 0.14 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,25 +1,4 @@
 function solution(progresses, speeds) {
     var answer = [];
-    let lastDates = [];
-    const N = progresses.length;
-    
-    for (let i=0; i < N; i++) {
-        lastDates.push(Math.ceil((100 - progresses[i]) / speeds[i]))
-    }
-    
-    let curr = 0;
-    let next = 1;
-    
-    while (curr < N) {
-        let cnt = 1;
-        while (next < N && lastDates[next] <= lastDates[curr]) {
-            cnt++;
-            next++;
-        }
-        answer.push(cnt);
-        curr = next;
-        next = curr + 1;
-    }
-        
     return answer;
 }
