d81504f859885ca6194ffdaace64da63e43fdecd
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-08-19 16:45:30 +0900

    [level 2] Title: 소수 찾기, Time: 1.36 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,46 +1,37 @@
+function dfs(str, status, numbers, isCheckBefore) {
+    let count = 0;
+    
+    if (str.length > 0) {
+        const num = Number(str); 
+       if (!isCheckBefore.has(num)) {
+           isCheckBefore.add(num);
+           if (isPrime(num)) count++;
+       }
+    }
+    
+    for (let i=0; i < numbers.length; i++) {
+        if (!status[i]) {
+            status[i] = true;
+            count += dfs(str + numbers[i], status, numbers, isCheckBefore);
+            status[i] = false;
+        }
+    }
+    
+    return count;
+}
+
+function isPrime(num) {
+    if (num < 2) return false;
+
+    for (let i=2; i * i <= num; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
 function solution(numbers) {
-    let answer = 0;
     const isCheckBefore = new Set();
+    const status = Array.from({length: numbers.length}, () => false);
     
-    const isPrime = (num) => {
-        if (num === 2) return true;
-        if (num < 2) return false;
-        
-        for (let i=2; i * i <= num; i++) {
-            if (num % i === 0) return false;
-        }
-        return true;
-    }
-    
-    const queue = [];
-    for (let i=0; i< numbers.length; i++) {
-        const num = numbers[i];
-        queue.push([num, Array.from({length: numbers.length}, (_, idx) => idx === i)]);
-    }
-    
-    let head = 0;
-    while (head < queue.length) {
-        const round = queue[head][0].length;
-        const num = +queue[head][0];
-        const status = queue[head][1];
-        head++;
-        
-        if (isCheckBefore.has(num)) continue;
-        isCheckBefore.add(num);
-        
-        if (isPrime(num)) answer++;
-        
-        if (round === numbers.length) continue;
-        
-        for (let i=0; i < numbers.length; i++) {
-            if (!status[i]) {
-                const newStatus = [...status];
-                newStatus[i] = true;
-                queue.push([num + numbers[i], newStatus]);
-            }
-        }
-        
-    }
-    
-    return answer;
+    return dfs("", status, numbers, isCheckBefore);
 }
