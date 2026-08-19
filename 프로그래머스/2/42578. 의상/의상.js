98468e440f74e92f3d5c710d36778f98cef1ff2f
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-08-19 18:17:59 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,22 +1,13 @@
 function solution(clothes) {
-    let answer = 0;
+    let answer = 1;
     const categoryList = new Map();
     for (let [_, category] of clothes) {
         categoryList.set(category, (categoryList.get(category) || 0) + 1)
     }
-    const list = [...categoryList.values()];
         
-    const dfs = (start, cases) => {         
-        for (let i = start; i < list.length; i++) {
-            const newCases = cases * list[i];
-
-            answer += newCases;
-            
-            dfs(i + 1, newCases);
-        }
-    }    
+    for (let i of categoryList.values()) {
+        answer *= (i + 1);
+    }
     
-    dfs(0, 1);
-    
-    return answer;
+    return answer - 1;
 }
