37ba3db56e6d6fc48711229252028b31a59dee41
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-08-19 16:20:05 +0900

    [level 2] Title: 소수 찾기, Time: 5.66 ms, Memory: 56.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,44 @@
+function solution(numbers) {
+    let answer = 0;
+    const isCheckBefore = new Set();
+    
+    const isPrime = (num) => {
+        if (num === 2) return true;
+        if (num < 2) return false;
+        
+        for (let i=2; i * i <= num; i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+    
+    const queue = [];
+    for (let i=0; i< numbers.length; i++) {
+        const num = numbers[i];
+        queue.push([num, Array.from({length: numbers.length}, (_, idx) => idx === i)]);
+    }
+    
+    let head = 0;
+    while (head < queue.length) {
+        const round = queue[head][0].length;
+        const num = +queue[head][0];
+        const status = queue[head][1];
+        head++;
+                
+        if (!isCheckBefore.has(num) && isPrime(num)) answer++;
+        isCheckBefore.add(num);
+        
+        if (round === numbers.length) continue;
+        
+        for (let i=0; i < numbers.length; i++) {
+            if (!status[i]) {
+                const newStatus = [...status];
+                newStatus[i] = true;
+                queue.push([num + numbers[i], newStatus]);
+            }
+        }
+        
+    }
+    
+    return answer;
+}
