e10cfcae511383a6047c924cf8ba99b7e09202da
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-08-21 00:00:25 +0900

    [level 1] Title: 최소직사각형, Time: 2.24 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,17 @@
+function pass(w, h, sizes) {
+    
+}
+
+function solution(sizes) {
+    let minW = 0;
+    let minH = 0;
+    for (let i=0; i < sizes.length; i++) {
+        if (sizes[i][0] >= sizes[i][1]) {
+            [sizes[i][0], sizes[i][1]] = [sizes[i][1], sizes[i][0]];
+        }
+        minW = Math.max(minW, sizes[i][0]);
+        minH = Math.max(minH, sizes[i][1]);
+    }
+    
+    return minW * minH;
+}
