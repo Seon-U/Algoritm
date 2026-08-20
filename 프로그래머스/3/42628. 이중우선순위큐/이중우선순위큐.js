3db5ee8610fa0d59b421fd879e901384cb516c98
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-08-20 22:14:02 +0900

    [level 3] Title: 이중우선순위큐, Time: 1868.98 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,19 +1,99 @@
-function solution(operations) {
-    let dq = [];
-    //10 ^ 6 1회
-    let max = 0, min = 0;
-    for (let i=0; i < operations.length; i++) {
-        const [op, t] = operations[i].split(" ");
-        switch (op) {
-            case "I": 
-                dq.push(Number(t));
-                // N log N (max 10 ^ 6 => 10 ^ 14, 터질 수 있음)
-                dq = dq.sort((a, b) => b - a);
-                break;
-            case "D":
-                // N
-                dq.splice(t > 0 ? 0 : -1, 1);
+class Heap {
+    constructor(compare) {
+        this.heap = [];
+        this.compare = compare;
+    }
+    
+    push(val) {
+        this.heap.push(val);
+        this._up(this.heap.length - 1);
+    }
+    
+    //logN
+    pop() {
+        if (this.heap.length === 0) return null;
+        if (this.heap.length === 1) return this.heap.pop();
+        
+        const top = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this._down(0);
+        return top;
+    }
+    
+    peek() {
+        return this.heap.length > 0 ? this.heap[0] : null;
+    }
+    
+    _up(i) {
+        while (i > 0) {
+            const parent = Math.floor((i - 1 / 2));
+            if (this.compare(this.heap[i], this.heap[parent]) <= 0) break;
+            [this.heap[i], this.heap[parent]] = [this.heap[parent], this.heap[i]];
+            i = parent;
+            
         }
     }
-    return dq.length > 0 ?[dq[0], dq[dq.length - 1]] : [0, 0]
+    
+    _down(i) {
+        const len = this.heap.length;
+        while (i * 2 + 1 < len) {
+            let left = i * 2 + 1;
+            let right = i * 2 + 2;
+            let target = left;
+            
+            if (right < len && this.compare(this.heap[right], this.heap[left]) > 0) {
+                target = right;
+            }
+            if (this.compare(this.heap[target], this.heap[i]) <= 0) break;
+            [this.heap[i], this.heap[target]] = [this.heap[target], this.heap[i]];
+            i = target;
+        }
+    }
+}
+
+
+
+
+function solution(operations) {
+    const maxHeap = new Heap((a, b) => a.val - b.val);
+    const minHeap = new Heap((a, b) => b.val - a.val);
+    
+    const visited = new Array(operations.length).fill(false);
+    
+    operations.forEach((op, id) => {
+        const [command, numStr] = op.split(' ');
+        const num = Number(numStr);
+        
+        if (command === "I") {
+            const node = { val: num, id };
+            maxHeap.push(node);
+            minHeap.push(node);
+            visited[id] = true;
+        } else if (num === 1) {
+            //Lazy deletion
+            while (maxHeap.peek() && !visited[maxHeap.peek().id]) {
+                maxHeap.pop();
+            }
+            if (maxHeap.peek()) {
+                visited[maxHeap.pop().id] = false;
+            }
+        } else if (num === -1) {
+            //Lazy deletion
+            while (minHeap.peek() && !visited[minHeap.peek().id]) {
+                minHeap.pop();
+            }
+            if (minHeap.peek()) {
+                visited[minHeap.pop().id] = false;
+            }
+        }
+    });
+    
+    while (maxHeap.peek() && !visited[maxHeap.peek().id]) maxHeap.pop();
+    while (minHeap.peek() && !visited[minHeap.peek().id]) minHeap.pop();
+    
+    if (!maxHeap.peek() || !minHeap.peek()) {
+        return [0, 0];
+    }
+    
+    return [maxHeap.peek().val, minHeap.peek().val];
 }
