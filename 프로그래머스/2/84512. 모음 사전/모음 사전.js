5362702a68034635d8c0f22e0a04d77434c0061a
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-08-22 18:49:11 +0900

    [level 2] Title: 모음 사전, Time: 1.09 ms, Memory: 44.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,29 +1,30 @@
-function solution(word) {    
-    // 반복하는 배열
-    const alphabet = ['A', 'E', 'I', 'O', 'U'];
-    let isFound = false;
-    // 상태값 (현재 w, dfs 재귀로 queue 상태 공유, answer, isUsed는 외부 공유)
-    const isUsed = new Set();
+const cache = new Map();
+
+let count = 0;
+const alphabet = ['A', 'E', 'I', 'O', 'U'];
+
+
+function solution(word) { 
+    if (cache.has(word)) return cache.get(word);
     
     const dfs = (w) => {
-        if (w === word) {
-            isFound = true; 
-            return;
+        if (w.length > 0 && !cache.has(w)) {
+            count++;
+            cache.set(w, count);
         }
         
-        if (w.length === 5) return;
+        if (w === word) return true;
+        if (w.length === 5) return false;
         
-        for (const a of alphabet) {
-            if (isFound) return;
+        for (const a of alphabet) {            
+            const newW = w + a;    
             
-            const newW = w + a;            
-            if (!isUsed.has(newW)) {
-                dfs(newW);
-                isUsed.add(newW);
-            }
+            if (dfs(newW)) return true;
         }
+        
+        return false;
     }
     
     dfs('');
-    return isUsed.size;
+    return cache.get(word);
 }
