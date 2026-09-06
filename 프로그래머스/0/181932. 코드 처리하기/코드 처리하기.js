890b1a9c25eef70acd4ec38b51a0b028fe620014
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-09-06 17:31:35 +0900

    [level 0] Title: 코드 처리하기, Time: 2.95 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,13 @@
+function solution(code) {
+    let answer = '';
+    let mode = false;
+    for (let i=0; i < code.length; i++) {
+        if (code[i] === "1") mode = !mode;
+        else if (mode && i % 2 !== 0) {
+            answer += code[i];
+        } else if (!mode && i % 2 === 0) {
+            answer += code[i];
+        } 
+    }
+    return answer.length === 0 ? "EMPTY" : answer;
+}
