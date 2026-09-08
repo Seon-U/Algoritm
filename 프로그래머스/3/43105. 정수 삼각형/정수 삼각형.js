5180c2f9d18dd93e7b86497995b8a4653afd9ec4
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-09-08 16:50:49 +0900

    [level 3] Title: 정수 삼각형, Time: 7.37 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,19 +1,16 @@
 // 1차 dfs 효율 실패
 // 2차 메모이제이션 추가
+// 3차 역순 조회 설정
 
 function solution(triangle) {
+    const newT = triangle.slice();
     const height = triangle.length;
-    const memo = Array.from({length: height}, () => Array(height).fill(-1));
     
-    const dfs = (h, i) => {
-        if (h === height - 1) return triangle[h][i];
-        
-        if (memo[h][i] !== -1) return memo[h][i];
-        
-        memo[h][i] = triangle[h][i] + Math.max(dfs(h + 1, i), dfs(h + 1, i + 1));
-        
-        return memo[h][i];
+    for (let i = height - 2; i >= 0; i--) {
+        for (let j = 0; j < newT[i].length; j++) {
+            newT[i][j] += Math.max(newT[i + 1][j], newT[i + 1][j + 1]);
+        }
     }
     
-    return dfs(0, 0);
+    return newT[0][0];
 }
