28d14fdff627653e89903f445cf635c24dca9bdb
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-09-20 21:18:53 +0900

    [level 4] Title: 도둑질, Time: 74.58 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.js
@@ -0,0 +1,21 @@
+function solution(money) {
+    const n = money.length;
+    
+    const dp1 = new Array(n).fill(0);
+    dp1[0] = money[0];
+    dp1[1] = money[0];
+    
+    for (let i = 2; i < n - 1; i++) {
+        dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + money[i]);
+    }
+    
+    const dp2 = new Array(n).fill(0);
+    dp2[0] = 0;
+    dp2[1] = money[1];
+    
+    for (let i = 2; i < n; i++) {
+        dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + money[i]);
+    }
+    
+    return Math.max(dp1[n - 2], dp2[n - 1]);
+}
