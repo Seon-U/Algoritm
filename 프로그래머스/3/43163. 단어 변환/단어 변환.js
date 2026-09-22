3ae20c0eed79994fc7babdc4b04cbd3e9cf13648
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-09-22 20:43:02 +0900

    [level 3] Title: 단어 변환, Time: 0.27 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,42 +1,41 @@
+// BFS, 그리고 그래프 특성 상 제일먼저 목적 달성이 가장 짧고, 
+// 뒷 순위 방문 시 그보다 먼저 방문할 경우가 더 최단거리임 (여러 길 중 최단거리)
 function solution(begin, target, words) {
-    let answer = 0;
     
-    // push pop이 더 안전할지도 (최대 50!)
+    if (!words.includes(target)) return 0;
+    
     const visited = new Set();
-    
-    // 초기값 세팅
-    // state (현재 단어, 이전 단어갔는지 아닌지 체크(순환제거), 현재까지 거쳐온 개수)
+
     visited.add(begin);
-    const queue = [[begin, visited]];
+    const queue = [[begin, 0]];
     
-    while (0 < queue.length) {
-        const [currWord, currVisited] = queue.shift();
+    let head = 0;
+    
+    while (head < queue.length) {
+        const [currWord, cnt] = queue[head];
 
         // 목적 달성 체크
-        if (currWord === target) {
-            const shiftCnt = currVisited.size - 1;
-            
-            if (answer === 0) answer = shiftCnt;
-            else if (answer > shiftCnt) answer = shiftCnt;
-            
-        } else {
-            // 넣어주기
-            for (let word of words) {
-                // 중복 순환 방지
-                if (currVisited.has(word)) continue;
+        if (currWord === target) return cnt;
 
-                // 1글자 차이 체크
-                let diffCnt = 0;
-                for (let i = 0; i < word.length; i++) {
-                    if (word[i] !== currWord[i]) ++diffCnt;
-                }
-                if (diffCnt > 1) continue;
+        // 넣어주기
+        for (let word of words) {
+            // 중복 순환 방지
+            if (visited.has(word)) continue;
 
-                const newVisited = new Set(currVisited).add(word);
-                queue.push([word, newVisited]);
-            }   
-        }
+            // 1글자 차이 체크
+            let diffCnt = 0;
+            for (let i = 0; i < word.length; i++) {
+                if (word[i] !== currWord[i]) diffCnt++;
+            }
+            
+            if (diffCnt === 1) {
+                visited.add(word);
+                queue.push([word, cnt + 1]);
+            }
+        }  
+        
+        head++;
     }
     
-    return answer;
+    return 0;
 }
