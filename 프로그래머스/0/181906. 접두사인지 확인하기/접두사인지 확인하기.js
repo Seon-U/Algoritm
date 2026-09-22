6a134d652acfa17e4d0232fc5f6fcf4e3d5ffe11
Seonu <113221248+Seon-U@users.noreply.github.com>
2026-09-22 20:49:36 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.09 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,10 +1,4 @@
 function solution(my_string, is_prefix) {
-    const N = is_prefix.length; 
-    if (N > my_string.length) return 0;
-    
-    for (let i = 0; i < is_prefix.length; i++) {
-        if (my_string[i] !== is_prefix[i]) return 0;
-    }
-    
-    return 1;
+    var answer = 0;
+    return answer;
 }
